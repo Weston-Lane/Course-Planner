38d546e2645cd66d0db8297af814ce1faab03e7d
Weston Lane <weston.r.lane@gmail.com>
2024-08-14 16:01:21 -0400

    Add project files.

---
+++ include/Log.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+class Log
+{
+public:
+	void print(int a, int b)
+	{
+		std::cout << a + " " + b;
+	}
+};
---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include <stdio.h>
+#include <iostream>
+
+
+#include "Log.hpp"
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Course Planner", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+    
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    int row = 5;
+    int col = 1;
+
+    while (!glfwWindowShouldClose(window))
+    {
+   
+        glfwPollEvents();
+        // Start the Dear ImGui frame
+
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::Begin("hello",nullptr, ImGuiWindowFlags_NoDecoration| ImGuiWindowFlags_NoMove| ImGuiWindowFlags_NoResize);
+
+        if (ImGui::Button("add column +"))
+            col++;
+        
+        if (ImGui::Button("add row +"))
+            row++;
+
+        ImGui::BeginTable("table", col);
+        for (int r = 0; r < row; r++)
+        {
+            ImGui::TableNextRow();
+            for (int c = 0; c < col; c++)
+            {
+                ImGui::TableSetColumnIndex(c);
+                ImGui::Text("row %d Col %d", r, c);
+            }
+        }
+        
+        ImGui::EndTable();
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
